a552587d53143ae5196b1ce899946059e7ef7b4d
Kishore270426 <kishorek2704@gmail.com>
2025-08-30 13:47:34 +0530

    updated

--- eventmanagement/settings.py
+++ eventmanagement/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'eventmanagement.wsgi.application'
 
 
-import os
+"""import os
 DATABASES = {
         "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -85,7 +85,15 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": "5432",
         }
-    }
+    }"""
+
+import os
+import dj_database_url
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
